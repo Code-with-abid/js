ac4475050c38cc4e87b7beaac648feb151377370
Abid <91589786+Code-with-abid@users.noreply.github.com>
2024-01-26 19:20:02 +0000

    data types discuss

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,6 +1,14 @@
 const accountId =12337
 let accountEmail="12345"
 var accountCity="jk"
+let accountState;
 
-accountId=2
+// accountId=2  not allowed
+/*
+Prefer not to use var 
+because of issue i block scope and function scope
+*/
+accountEmail="sajj.com"
+accountCity="jammu"
 console.log(accountId);
+console.table([accountId,accountEmail,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict";// treat all js code as newer version
+ //alert(3+4)// we are using react , not browser.
+
+ let name="Abid"
+ let age=24
+ let isLoggedIn =false 
+ 
+
+
+ //number => 2 to power 53
+ // bgint
+ // string => ""
+ // boolean => true/false
+ // null => standalone value
+  //undefiend => 
+  // symbol =>unique
+  
+  
+
+  //object
+
+  console.log(typeof null) //object
