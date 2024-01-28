8dbae7743353b11a0e0e0d60b7b639d78f952137
Abid <91589786+Code-with-abid@users.noreply.github.com>
2024-01-28 19:25:37 +0000

    some operations covered

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -20,6 +20,38 @@
 // let booleanIsLoggedIn=Boolean(isLoggedIn)
 // console.log(booleanIsLoggedIn);
 let someNumber=33
-let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log( typeof stringNumber);
+// let stringNumber=String(someNumber)
+// console.log(stringNumber);
+// console.log( typeof stringNumber);
+
+
+
+////////**************************operations**********************/////////
+
+// let  value=34
+// let negValue=-value
+
+// console.log(negValue);
+
+// console.log(2+4);
+// console.log(2-4);
+// console.log(2*4);
+// console.log(2**4);
+// console.log(2/4);
+// console.log(2%4);
+ let str1="abid"
+ let str2 =" amin"
+ let str3=str1 + str2
+//  console.log(str3);
+// console.log(1+"2");
+// console.log("2"+6);
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log(true);
+let gameCounter=100
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+
+
