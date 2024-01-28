deca052987e237b2b6b6907e606170e1c053b442
Abid <91589786+Code-with-abid@users.noreply.github.com>
2024-01-28 18:58:12 +0000

    conversion of data types

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,4 +1,25 @@
-let score ="33"
+// let score ="abid"
 
-console.log(typeof score);
+// console.log(typeof score);
+
+
+
 //console.log(typeof(score));//
+// 33=> number //
+//"33abc"=> NaN//
+// true=>1  , false=>0
+
+// let valueInNumber=Number(score);
+// console.log( valueInNumber);
+// let isLoggedIn="abid"
+// 1=>true
+//0=> false
+//"abid"=> true
+//""=> false
+// let isLoggedIn=""
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log( typeof stringNumber);
